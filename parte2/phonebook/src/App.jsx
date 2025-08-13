6ad702e54b076e173e27d17e1c80bf8bb18f3c8c
dinorac22 <dinorac22@gmail.com>
2025-08-13 18:14:35 -0500

    Ejercicio 2.12 - parte 2

--- parte2/phonebook/src/App.jsx
+++ parte2/phonebook/src/App.jsx
@@ -30,9 +30,14 @@ function App() {
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1,
     };
-    setPersons(persons.concat(personObject));
+
+    axios
+      .post("http://localhost:3001/persons", personObject)
+      .then((response) => {
+        setPersons(persons.concat(response.data));
+      });
+
     setNewName("");
     setNewNumber("");
   };
